b817a04993613994bc6f17a517114874e6c6f903
Luis Fernando Santuario Parra <santuarioparral@hotmail.com>
2017-10-05 19:54:10 -0500

    Lab04

---
+++ PCLProyect/AppValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLProyect
+{
+    public class AppValidator
+    {
+        IDialog Dialog;
+        public AppValidator(IDialog plataformDialog) {
+            Dialog = plataformDialog;
+        }
+
+        public string EMail{ get; set; }
+        public string Password { get; set; }
+        public string Device { get; set; }
+
+        public async void Validate() {
+
+            string Result;
+            Result = "Aplicación Validada";
+            Dialog.Show(Result);
+            var ServiceClient = new SALLab04.ServiceClient();
+            var SvcResult = await ServiceClient.ValidateAsync(EMail,Password,Device);
+            Result = $"{SvcResult.Status}\n{SvcResult.Fullname}\n{SvcResult.Token}";
+            Dialog.Show(Result);
+        }
+    }
+}
---
+++ UWPApp/UPWDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPApp
+{
+    class UPWDialog:PCLProyect.IDialog
+    {
+        public async void Show(string messange) {
+            var Dialog = new Windows.UI.Popups.MessageDialog(messange);
+            await Dialog.ShowAsync();
+
+        }
+
+    }
+}
